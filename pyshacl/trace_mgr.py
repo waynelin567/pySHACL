d41032035c74f32b610f52de6657d97c8d3ace0f
waynelin567 <waynelin567@gmail.com>
2024-06-25 10:57:03 -0700

    use uri to index shape containers in TraceMgr

--- pyshacl/trace_mgr.py
+++ pyshacl/trace_mgr.py
@@ -5,6 +5,9 @@ class ShapeContainer:
         self._shape_name = shape_name
         self._traces:list[Trace] = []
         self.shacl_syntax = shacl_syntax
+        self._shape_uri_name = None
+    def set_shape_uri_name(self, shape_uri_name:str):
+        self._shape_uri_name = shape_uri_name
     def add_trace(self, trace:Trace):
         self._traces.append(trace)
     def print(self):
@@ -22,7 +25,12 @@ class TraceMgr:
             cls._instance = super(TraceMgr, cls).__new__(cls)
         return cls._instance
     def add_shape_container(cls, shape_name:str, sc:ShapeContainer):
-        cls._shapes[shape_name] = sc 
+        # for some reason this shape_uri_name is hard to get from shape.py
+        # it looks something like nc89b5fc355c0437d99ebb586ed03bbdbb1
+        # or urn:my_site_constraints/return-air-temperature2
+        shape_uri_name = shape_name.split('>')[0].strip('<>').split("Shape")[1].strip()
+        sc.set_shape_uri_name(shape_uri_name)
+        cls._shapes[shape_uri_name] = sc 
     def print(cls):
         for shape_name, sc in cls._shapes.items():
             print("="*25,f"Shape: {shape_name}", "="*25)
@@ -31,3 +39,6 @@ class TraceMgr:
     def get_focus_neighbors(cls, graph:Graph):
         for shape_name, sc in cls._shapes.items():
             sc.get_focus_neighbors(graph)
+    def get_shape(cls, shape_uri_name:str):
+        assert shape_uri_name in cls._shapes, f"Shape {shape_uri_name} not found"
+        return cls._shapes[shape_uri_name]
